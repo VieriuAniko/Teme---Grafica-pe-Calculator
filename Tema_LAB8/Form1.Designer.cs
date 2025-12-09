9db92732d1bdb54c1b09e2cf98ce629c907e5eff
VieriuAniko <146959409+VieriuAniko@users.noreply.github.com>
2025-12-09 21:21:49 +0200

    Add files via upload

---
+++ Tema_LAB8/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Tema_LAB8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxFern = new System.Windows.Forms.PictureBox();
+            this.trackBarPoints = new System.Windows.Forms.TrackBar();
+            this.labelPoints = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFern)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxFern
+            // 
+            this.pictureBoxFern.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxFern.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxFern.Name = "pictureBoxFern";
+            this.pictureBoxFern.Size = new System.Drawing.Size(1775, 746);
+            this.pictureBoxFern.TabIndex = 0;
+            this.pictureBoxFern.TabStop = false;
+            // 
+            // trackBarPoints
+            // 
+            this.trackBarPoints.Location = new System.Drawing.Point(1276, 35);
+            this.trackBarPoints.Maximum = 100000;
+            this.trackBarPoints.Minimum = 1000;
+            this.trackBarPoints.Name = "trackBarPoints";
+            this.trackBarPoints.Size = new System.Drawing.Size(463, 56);
+            this.trackBarPoints.TabIndex = 1;
+            this.trackBarPoints.TickFrequency = 5000;
+            this.trackBarPoints.Value = 20000;
+            // 
+            // labelPoints
+            // 
+            this.labelPoints.AutoSize = true;
+            this.labelPoints.Location = new System.Drawing.Point(1273, 146);
+            this.labelPoints.Name = "labelPoints";
+            this.labelPoints.Size = new System.Drawing.Size(89, 16);
+            this.labelPoints.TabIndex = 2;
+            this.labelPoints.Text = "Puncte: 20000";
+            this.labelPoints.Click += new System.EventHandler(this.labelPoints_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1775, 746);
+            this.Controls.Add(this.labelPoints);
+            this.Controls.Add(this.trackBarPoints);
+            this.Controls.Add(this.pictureBoxFern);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFern)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxFern;
+        private System.Windows.Forms.TrackBar trackBarPoints;
+        private System.Windows.Forms.Label labelPoints;
+    }
+}
+
---
+++ Tema_LAB8/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema_LAB8
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Evenimente
+            this.Load += Form1_Load;
+            trackBarPoints.Scroll += trackBarPoints_Scroll;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setam textul label-ului la incarcare
+            labelPoints.Text = $"Puncte: {trackBarPoints.Value}";
+            DrawFern();
+        }
+
+        private void trackBarPoints_Scroll(object sender, EventArgs e)
+        {
+            labelPoints.Text = $"Puncte: {trackBarPoints.Value}";
+            DrawFern();
+        }
+
+        private void DrawFern()
+        {
+            int width = pictureBoxFern.Width;
+            int height = pictureBoxFern.Height;
+
+            if (width <= 0 || height <= 0)
+                return;
+
+            int n = trackBarPoints.Value;
+
+            Bitmap bmp = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Black);
+            }
+
+            // Coordonate pentru Barnsley Fern
+            double x = 0.0;
+            double y = 0.0;
+
+            // Dimensiunile aproximative ale ferigii
+            // x ~ [-2.5, 2.5], y ~ [0, 10]
+            double scaleX = width / 6.0;   // puțin mai lat decât intervalul [-2.5, 2.5]
+            double scaleY = height / 10.0; // înălțime 0..10
+
+            for (int i = 0; i < n; i++)
+            {
+                double r = rand.NextDouble();
+                double x1, y1;
+
+                // Cele 4 transformări din Barnsley Fern
+                if (r < 0.01)
+                {
+                    // T1
+                    x1 = 0.0;
+                    y1 = 0.16 * y;
+                }
+                else if (r < 0.86)
+                {
+                    // T2
+                    x1 = 0.85 * x + 0.04 * y;
+                    y1 = -0.04 * x + 0.85 * y + 1.6;
+                }
+                else if (r < 0.93)
+                {
+                    // T3
+                    x1 = 0.20 * x - 0.26 * y;
+                    y1 = 0.23 * x + 0.22 * y + 1.6;
+                }
+                else
+                {
+                    // T4
+                    x1 = -0.15 * x + 0.28 * y;
+                    y1 = 0.26 * x + 0.24 * y + 0.44;
+                }
+
+                x = x1;
+                y = y1;
+
+                // Convertim din coordonate matematice la pixeli
+                int px = (int)(width / 2 + x * scaleX);
+                int py = (int)(height - y * scaleY); // inversăm axa Y
+
+                if (px >= 0 && px < width && py >= 0 && py < height)
+                {
+                    Color c = GetGradientColor(i, n);
+                    bmp.SetPixel(px, py, c);
+                }
+            }
+
+            pictureBoxFern.Image = bmp;
+        }
+
+        // Gradient simplu de la DarkGreen la LightGreen
+        private Color GetGradientColor(int index, int total)
+        {
+            if (total <= 1)
+                return Color.LightGreen;
+
+            double t = (double)index / (total - 1);
+
+            Color start = Color.DarkGreen;
+            Color end = Color.LightGreen;
+
+            int r = (int)(start.R + t * (end.R - start.R));
+            int g = (int)(start.G + t * (end.G - start.G));
+            int b = (int)(start.B + t * (end.B - start.B));
+
+            // Asigurăm limitele în 0..255
+            r = Math.Max(0, Math.Min(255, r));
+            g = Math.Max(0, Math.Min(255, g));
+            b = Math.Max(0, Math.Min(255, b));
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private void labelPoints_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
